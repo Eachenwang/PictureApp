59295400a5b109a5930ef9be908653b21b5de1b7
wangyichen <ethan.wake@gmail.com>
2018-08-07 15:12:07 +0800

    11-4 手勢滑動更換圖檔

--- app/src/main/java/com/eth/pictureapp/DetailActivity.java
+++ app/src/main/java/com/eth/pictureapp/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.eth.pictureapp;
 
+import android.accessibilityservice.GestureDescription;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -7,10 +8,13 @@ import android.provider.MediaStore;
 import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
+    GestureDetector detector;
     private int position;
     private ImageView image;
     private Cursor cursor;
@@ -19,6 +23,8 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        detector = new GestureDetector(this, this);
 
         position = getIntent().getIntExtra("POSITION", 0);
         image = findViewById(R.id.imageView);
@@ -36,4 +42,56 @@ public class DetailActivity extends AppCompatActivity {
         Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
         image.setImageBitmap(bitmap);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return detector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent1, MotionEvent motionEvent2, float v, float v1) {
+        float distance = motionEvent2.getX()-motionEvent1.getX();
+        if (distance > 100) {
+            //往右 往前一張
+            if (!cursor.moveToPrevious()) {
+                cursor.moveToLast();
+            }
+            updateImage();
+        } else if (distance < 100) {
+            if (!cursor.moveToNext()) {
+                cursor.moveToFirst();
+            }
+            updateImage();
+        }
+
+        return false;
+    }
 }
+
+
+
